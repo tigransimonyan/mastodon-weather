2345e518ab18611876a4e826497b9c5aebc54981
Tigran <tigran.contact@gmail.com>
2021-08-15 02:19:57 +0400

    bug fix

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const megalodon = require('megalodon');
 const generator = megalodon.default;
-const client = generator('mastodon', BASE_URL, ACCESS_TOKEN);
 const http = require('http');
 
 const BASE_URL = 'https://xn--y9a6bah4ck.xn--y9a3aq';
 const ACCESS_TOKEN = process.env.ACCESS_TOKEN;
 
-var options = {
+const client = generator('mastodon', BASE_URL, ACCESS_TOKEN);
+const options = {
   host: 'wttr.in',
   path: encodeURI(
     '/Yerevan,%20Armenia?lang=hy&format=Դրսում %t է բայց կարծես %f լինի։\n%C %cէ։\n\nԻ՞նչ նպատակ ունես այսօր։'
